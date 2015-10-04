847054874aae1db69c0dbbc1fa03c64a9c2a6c35
Billy <ibly31productions@gmail.com>
2015-10-04 00:25:05 -0500

    CV2 support

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ from threading import Thread
 import time
 from datetime import datetime
 import picamera
-import picamera.array
-import io
+from picamera.array import PiRGBArray
+import cv2
 
 class VideoThread(Thread):
 	def __init__(self):
@@ -12,22 +12,26 @@ class VideoThread(Thread):
 		self.camera = picamera.PiCamera()
 		self.camera.vflip = True
 		self.camera.framerate = 30
-		self.camera.resolution = (1280, 720)
+		self.camera.resolution = (640, 480)
 		self.camera.use_video_port = True
 		self.camera.quality = 85
 
+		self.rawCapture = PiRGBArray(self.camera, size=(640, 480))
+
 	def run(self):
 		self.camera.start_preview()
-		time.sleep(2)
+		time.sleep(1) # warm up the camera
 
-		i = 0
-		while True:
-			#self.camera.capture("app/frame%d.jpeg" % i)	
-			print "Capture %d at %s\n" % ( i, str(datetime.now()) )
-			i += 1
-			time.sleep(0.01)
-		
-		self.camera.stop_preview()	 
+		for frame in camera.capture_continuous(self.rawCapture, format="bgr", user_video_port=True):
+			image = frame.array
+
+			cv2.imshow("Frame", image)
+			key = cv2.waitKey(1) & 0xFF
+
+			self.rawCapture.truncate(0)
+
+			if key == ord("q"):
+				break
 
 class ServerThread(Thread):
 	def __init__(self):
